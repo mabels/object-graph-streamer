07f3060decb3d4cee583e5980e0e2acf2a55b063
Meno Abels <meno.abels@adviser.com>
2021-12-13 20:13:38 +0100

    chore: smoke.py fix

--- smoke/smoke.py
+++ smoke/smoke.py
@@ -1,17 +1,18 @@
 from dataclasses import dataclass
 
-from object_graph_streamer as ogs
+import object_graph_streamer as ogs
 
 @dataclass
 class Test:
   Yoo: int
   Bla: int
 
-sample = Test(Yoo=9, Bla= 5)
-out = ""
-jsonC = ogs.NewJsonCollector(lambda o: out += o)
-ogs.ObjectGraphStreamer(sample, lambda prob: jsonC.Append(prob))
-if out != "{\"Bla\":5,\"Yoo\":9}":
+
+sample ={"Yoo":9, "Bla": 5}
+out = []
+jsonC = ogs.JsonCollector(lambda o: out.append(o))
+ogs.objectGraphStreamer(sample, lambda prob: jsonC.append(prob))
+if "".join(out) != "{\"Bla\":5,\"Yoo\":9}":
   raise Exception(f'out={out}')
 
 print("Ready for production")
